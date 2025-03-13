c986074835fa6e6a451aab66c1a15da1ef1461b3
AlexL626 <alexl626tg@gmail.com>
2025-03-13 16:38:20 +0800

    deploy version 0.4.2,del filebeat-cm

--- lambda-argocd-trigger/create_secret.sh
+++
@@ -1,6 +0,0 @@
-kubectl create secret generic lambda-trigger-config \
-  --namespace argocd \
-  --from-literal=ARGOCD_TOKEN=$ARGOCD_TOKEN \
-  --from-literal=AWS_REGION=us-west-2 \
-  --from-literal=LOG_LEVEL=INFO
-  --from-literal=LAMBDA_ARN="arn:aws:lambda:us-west-2:590184059249:function:lambda-argocd-trigger"
